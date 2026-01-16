f6183a2e3ba80a03c0762cacc8e2d9887ba7f622
AMaraza <allymaraza@outlook.com>
2026-01-16 14:35:00 -0500

    Finished styling

--- script.js
+++ script.js
@@ -20,4 +20,27 @@ function RotateViewport() {
         resizableContainer.style.width = currentHeight;
         resizableContainer.style.height = currentWidth;
     }
+    else {
+        const buttons = document.querySelectorAll('.resize');
+        buttons.forEach(button => {
+            if (button.id == "mobile" || button.id == "tablet") {
+                button.style.backgroundColor = "lightgreen";
+            }
+            else if (button.id=="desktop" || button.id == "laptop") {
+                button.style.backgroundColor = "red";
+            }
+        })
+
+        setTimeout(() => {
+            buttons.forEach(button => {button.style.backgroundColor = "lightgray";})
+        }, 2000);
+    }
+}
+
+function ChangeTheme() {
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute('data-theme');
+
+    const nextTheme = currentTheme === "dark" ? "light" : "dark";
+    root.setAttribute('data-theme', nextTheme);
 }
